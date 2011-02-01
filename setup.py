0da4d4c4a40913b704818eba833e1599ac5bf4a8
Pedro Algarvio <pedro@algarvio.me>
2011-02-01 00:36:53 +0000

    Add `.gitignore` and the library logo.

--- setup.py
+++ setup.py
@@ -13,27 +13,22 @@
 from setuptools import setup, find_packages
 import ircliblet
 
-setup(name=ircliblet.__package_name__,
-      version=ircliblet.__version__,
-      author=ircliblet.__author__,
-      author_email=ircliblet.__email__,
-      url=ircliblet.__url__,
-      download_url='http://python.org/pypi/%s' % ircliblet.__package_name__,
-      description=ircliblet.__summary__,
-      long_description=ircliblet.__description__,
-      license=ircliblet.__license__,
-      platforms="OS Independent - Anywhere Eventlet is known to run.",
+setup(name = ircliblet.__package_name__,
+      version = ircliblet.__version__,
+      author = ircliblet.__author__,
+      author_email = ircliblet.__email__,
+      url = ircliblet.__url__,
+      download_url = 'http://python.org/pypi/%s' % ircliblet.__package_name__,
+      description = ircliblet.__summary__,
+      long_description = ircliblet.__description__,
+      license = ircliblet.__license__,
+      platforms = "OS Independent - Anywhere Eventlet is known to run.",
       keywords = "Eventlet IRC Library",
       packages = find_packages(),
-      include_package_data = True,
-      package_data = {
-        'ircliblet': ['*.cfg']
-      },
       install_requires = [
-        "Distribute", "giblets>=0.2.1", "blinker>=1.1",
-        "pyzmq>=2.1.0,==2.1.0dev",
+        "Distribute", "eventlet>=0.9.14", "blinker>=1.1"
       ],
-      classifiers=[
+      classifiers = [
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: BSD License',
